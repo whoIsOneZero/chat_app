bd7169430cc53e4f535ecb40af572e62d5941d97
whoIsOneZero <oh.shalom.0@gmail.com>
2023-05-07 17:28:46 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import '../widgets/chat.dart';
+import '../widgets/messages.dart';
+import '../widgets/side_menu.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChitChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+        brightness: Brightness.light,
+        iconTheme: const IconThemeData().copyWith(color: Colors.blueGrey),
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Montserrat',
+        textTheme: const TextTheme(
+            displayMedium: TextStyle(
+              color: Colors.black,
+              fontSize: 32.0,
+              fontWeight: FontWeight.bold,
+            ),
+            headlineMedium: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black45,
+              letterSpacing: 2.0,
+            ),
+            bodyLarge: TextStyle(
+              color: Colors.black45,
+              fontSize: 14.0,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+            ),
+            bodyMedium: TextStyle(
+              color: Colors.black45,
+              letterSpacing: 1.0,
+            )),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.light,
+      home: const Shell(),
+    );
+  }
+}
+
+class Shell extends StatelessWidget {
+  const Shell({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+      //title: const Text("Home"),
+      //),
+      body: Column(
+        children: [
+          SideMenu(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/side_menu.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import '../widgets/chat.dart';
+import '../widgets/messages.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        width: 220.0,
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade300, width: 1.0)),
+        //color: Theme.of(context).primaryColor,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border(
+                bottom: BorderSide(color: Colors.grey.shade300, width: 1.0),
+              )),
+              child: Row(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Image.asset(
+                        'assets/chat_logo.png',
+                        height: 55.0,
+                        width: 30.0,
+                        filterQuality: FilterQuality.high,
+                      )),
+                  Row(children: [
+                    Text('ChitChat',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(fontSize: 20.0))
+                  ]),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            _SideMenuIconTab(
+              iconData: Icons.dashboard_outlined,
+              title: 'Dashboard',
+              onTap: () {},
+            ),
+            _SideMenuIconTab(
+              iconData: Icons.analytics_outlined,
+              title: 'Analytics',
+              onTap: () {},
+            ),
+            _SideMenuIconTab(
+              iconData: Icons.folder_copy_outlined,
+              title: 'Files',
+              onTap: () {},
+            ),
+            _SideMenuIconTab(
+              iconData: Icons.call_outlined,
+              title: 'Call',
+              onTap: () {},
+            ),
+            const SizedBox(height: 9.0),
+            _SideMenuIconTab(
+              iconData: Icons.message_outlined,
+              title: 'Messages',
+              onTap: () {},
+            ),
+            _SideMenuIconTab(
+              iconData: Icons.people_alt_outlined,
+              title: 'Community',
+              onTap: () {},
+            ),
+            _SideMenuIconTab(
+              iconData: Icons.settings,
+              title: 'Settings',
+              onTap: () {},
+            ),
+            /*Expanded(
+              child: Container(
+                alignment: Alignment.bottomLeft,
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 20,
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundImage: AssetImage(
+                        'assets/user.png',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )*/
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black,
+                        radius: 20,
+                        child: CircleAvatar(
+                          radius: 16,
+                          backgroundImage: AssetImage(
+                            'assets/user.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    child: const Padding(
+                        padding: EdgeInsets.only(left: 8.0, bottom: 20.0),
+                        child: Text(
+                          'User',
+                          style: TextStyle(color: Colors.black),
+                        )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SideMenuIconTab extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  final VoidCallback onTap;
+
+  const _SideMenuIconTab(
+      {Key? key,
+      required this.iconData,
+      required this.onTap,
+      required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        iconData,
+        color: Theme.of(context).iconTheme.color,
+        size: 28.0,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyLarge,
+        overflow: TextOverflow.ellipsis,
+      ),
+      onTap: onTap,
+    );
+  }
+}
