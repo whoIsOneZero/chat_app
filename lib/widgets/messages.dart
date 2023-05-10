649c6be77292ca47597f423075a73ed6c04814a5
whoIsOneZero <oh.shalom.0@gmail.com>
2023-05-10 12:05:44 +0000

    update

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -7,7 +7,6 @@ class Messages extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          //color: Colors.blue,
           border: Border.all(color: Colors.grey.shade300, width: 1.0)),
       child: Column(
         children: [
@@ -17,29 +16,43 @@ class Messages extends StatelessWidget {
                 child: Container(
                   height: 88.0,
                   decoration: BoxDecoration(
+                      //color: Colors.amberAccent,
                       border: Border(
                     bottom: BorderSide(color: Colors.grey.shade300, width: 1.0),
                   )),
                   child: Padding(
-                    padding: const EdgeInsets.all(16.0),
+                    padding: const EdgeInsets.only(top: 16.0),
                     child: ListTile(
-                      leading: Text(
-                        'Messages',
-                        style: Theme.of(context)
-                            .textTheme
-                            .displayMedium!
-                            .copyWith(color: Colors.blue, fontSize: 20.0),
+                      leading: Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: Text(
+                          'Messages',
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayMedium!
+                              .copyWith(
+                                  color: Colors.blueAccent[700],
+                                  fontSize: 22.0),
+                        ),
                       ),
                       trailing: Row(
                         mainAxisSize: MainAxisSize.min,
-                        children: const [
+                        children: [
                           Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.edit_outlined),
+                            padding: const EdgeInsets.only(
+                                bottom: 4.0, right: 6.0, top: 4.0),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.blueGrey[50],
+                              child: const Icon(Icons.edit_outlined),
+                            ),
                           ),
                           Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.search_outlined),
+                            padding: const EdgeInsets.only(
+                                bottom: 4.0, right: 1.0, top: 4.0),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.blueGrey[50],
+                              child: const Icon(Icons.search_outlined),
+                            ),
                           ),
                         ],
                       ),
