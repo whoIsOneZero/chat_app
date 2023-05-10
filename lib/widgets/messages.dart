4ed6b3eca04fdbfe6902765c7c837d12e1a406c3
whoIsOneZero <oh.shalom.0@gmail.com>
2023-05-10 10:12:26 +0000

    update

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -9,7 +9,45 @@ class Messages extends StatelessWidget {
       decoration: BoxDecoration(
           //color: Colors.blue,
           border: Border.all(color: Colors.grey.shade300, width: 1.0)),
-      child: Column(),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ListTile(
+                    leading: Text(
+                      'Messages',
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium!
+                          .copyWith(color: Colors.blue, fontSize: 20.0),
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(Icons.edit_outlined),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(Icons.search_outlined),
+                        ),
+                      ],
+                    ),
+                    /*trailing: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.edit_outlined),
+                    ),*/
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
